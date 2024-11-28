5047594c3b54519e236cf17cef197bad7fa92e8f
Mehtab Rosul <102592487+MehtabRosul@users.noreply.github.com>
2024-11-29 01:55:18 +0530

    Update Contact.tsx

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -12,12 +12,6 @@ const INITIAL_STATE = Object.fromEntries(
   Object.keys(config.contact.form).map((input) => [input, ""])
 );
 
-const emailjsConfig = {
-  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-  accessToken: import.meta.env.VITE_EMAILJS_ACCESS_TOKEN,
-};
-
 const Contact = () => {
   const formRef = useRef<React.LegacyRef<HTMLFormElement> | undefined>();
   const [form, setForm] = useState(INITIAL_STATE);
